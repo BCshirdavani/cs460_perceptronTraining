7823f8296f6cc025ba91f54046c8a8c8eb53dd03
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-10-29 16:34:28 -0700

    initial commit
    to-do:
     - update weights, row 62

---
+++ src/App.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Perceptron Training
+ */
+public class App
+{
+
+
+	//    private static final String linearlySeparableData = "C:\\BellevueCollege\\Courses\\CS460\\Examples\\ANNs\\TrainingData\\PerceptronTrainingData_LinearlySeparable.txt";
+//    private static final String linearlyInseparableData = "C:\\BellevueCollege\\Courses\\CS460\\Examples\\ANNs\\TrainingData\\DeltaRule_NotLinearlySeparable.txt";
+	private static final String linearlySeparableData = "/Users/shymacbook/Documents/BC/cs460_ML/Practice/Perceptron/PracticeMaterials/PerceptronTraining/PerceptronTrainingData_LinearlySeparable.txt";
+	private static final String linearlyInseparableData = "/Users/shymacbook/Documents/BC/cs460_ML/Practice/Perceptron/PracticeMaterials/PerceptronTraining/DeltaRule_NotLinearlySeparable.txt";
+	public static void main( String[] args )
+	{
+		try
+		{
+			System.out.println("~~~ hello world");
+			String[] trainingInstances = readFromFile(linearlySeparableData);
+
+			train(trainingInstances);
+			System.out.println("DONE");
+		}
+		catch(Exception ex)
+		{
+			System.out.println(String.format("ERROR: %s", ex.getMessage()));
+		}
+	}
+
+	private static void train(String[] trainingData)
+	{
+		double w0 = 0.01;
+		double w1 = 0.01;
+		double w2 = 0.01;
+
+		double learningRate = 0.001;  // NOTE:  try different values for this
+
+		System.out.println(String.format("Initializing weights to:  w0=%.2f, w1=%.2f, w2=%.2f", w0, w1, w2));
+		System.out.println(String.format("learningRate=%f", learningRate));
+
+		double x1;
+		double x2;
+		double target;
+		// Your training code goes here
+		int missCount = 0;
+		do{
+			for (int i = 0; i <= trainingData.length; i++){
+				String thisLine = trainingData[i];
+				String[] token = thisLine.split(",");
+				x1 = Double.parseDouble(token[0]);
+				x2 = Double.parseDouble(token[1]);
+				target = Double.parseDouble(token[2]);
+				System.out.println("string:\t" + thisLine + "\t=\tx1: " + x1 + "\tx2: " + x2 + "\ttarget: " + target);
+
+				double O_calc = (w0 * 1) + (w1 * x1) + (w2 * x2);
+
+				if (O_calc != target){
+					// ... update delta weights here...
+
+
+					missCount ++;
+				}
+			}
+		} while (missCount > 0);
+
+
+
+		System.out.println(String.format("Finished training:  w0=%f, w1=%f, w2=%f", w0, w1, w2));
+
+		double slope = -w1 / w2;
+		double intercept = -w0 / w2;
+
+		System.out.println(String.format("Equation of boundery: X2 = %f X1 + %f", slope, intercept));
+	}
+
+
+
+
+
+	private static String[] readFromFile(String path) throws FileNotFoundException, IOException
+	{
+		ArrayList<String> list = new ArrayList<String>();
+
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String line = br.readLine();
+		while (line != null) {
+			list.add(line);
+			line = br.readLine();
+		}
+
+		br.close();
+
+		String[] trainingData = list.toArray(new String[0]);
+
+		System.out.println(String.format("Training data (%d instances) read from file %s", trainingData.length, path));
+		return trainingData;
+	}
+
+}
